f3fa9b382b2a98fb6a00de77c319fc8d6fdaf9fc
Max Marriott-Clarke <maxmarriottclarke@me.com>
2026-06-30 20:27:55 +0200

    Added files needed to run the benchmarks

---
+++ build/CMakeFiles/sin_lut.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sin_lut.dir/sin_lut.cpp.o"
+  "CMakeFiles/sin_lut.dir/sin_lut.cpp.o.d"
+  "sin_lut"
+  "sin_lut.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sin_lut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
